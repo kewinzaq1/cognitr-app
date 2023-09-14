2db759419e1047aa1c6e4a90dd439a8689e30f05
Kewin Szlezingier <k.szlezingier@contina.pl>
2023-09-14 17:22:44 +0200

    chore: setup project

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,14 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const {getDefaultConfig} = require('expo/metro-config')
 
 /** @type {import('expo/metro-config').MetroConfig} */
+// eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname, {
   // [Web-only]: Enables CSS support in Metro.
   isCSSEnabled: true,
-});
+})
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs', 'cjs']
 
-module.exports = config;
+// eslint-disable-next-line no-undef
+module.exports = config
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
